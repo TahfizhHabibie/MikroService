3c7d69e242fd72854b5a54fd9edec591177f186c
Tahfizh Habibie <tahfizhhabibie122@gmail.com>
2026-07-26 08:25:25 +0700

    Add files via upload

---
+++ produk/src/main/java/com/habibi/produk/repository/ProdukRepository.java
@@ -0,0 +1,12 @@
+package com.habibi.produk.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.habibi.produk.model.Produk;
+
+@Repository
+public interface ProdukRepository extends JpaRepository<Produk, Long>{
+
+    
+}
